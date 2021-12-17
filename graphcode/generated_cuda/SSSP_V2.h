5b51f24efa0f3f4110f6a521bb5d2feacc005e2d
Ashwina <kumarashwina31@gmail.com>
2021-12-17 15:16:18 +0530

    SSSP prototype update

--- graphcode/generated_cuda/SSSP_V2.h
+++ graphcode/generated_cuda/SSSP_V2.h
@@ -7,22 +7,26 @@
 #include"../graph.hpp"
 #include"../libcuda.cuh"
 
-__global__ void void Compute_SSSP_kernel(graph& g,int* dist,int src);
+__global__ void void Compute_SSSP_kernel(int * gpu_OA , int * gpu_edgeList , int* gpu_weight, int * gpu_dist , int src ,int V, int MAX_VAL , bool * gpu_modified_prev,
+bool * gpu_modified_next, bool * gpu_finished)
+{
 
-graph& g,int* dist,int srcunsigned int id = threadIdx.x + (blockDim.x * blockIdx.x);
-unsigned int v =id;
-if (id < V)
-{if (modified[v] == true )for (int edge = gpu_OA[id]; edge < gpu_OA[id+1]; edge ++) 
-  {int nbr = g.edgeList[edge] ;
-    int e = edge;
-     int dist_new = dist[v] + weight[e];
-    bool modified_new = true;
-    if(dist[id]!= MAX_VAL && dist[nbr] > dist_new)
-    {
-      atomicMin(&dist[nbr],dist_new);
-      modified_nxt[nbr] = modified_new;
-      finished[0] = false ;
+  unsigned int id = threadIdx.x + (blockDim.x * blockIdx.x);
+  unsigned int v =id;
+  if (id < V)
+  {if (modified[v] == true )for (int edge = gpu_OA[id]; edge < gpu_OA[id+1]; edge ++) 
+    {int g = gpu_edgeList[edge] ;
+      int e = edge;
+       int dist_new = dist[v] + weight[e];
+      bool modified_new = true;
+      if(dist[id]!= MAX_VAL && dist[nbr] > dist_new)
+      {
+        atomicMin(&dist[nbr],dist_new);
+        modified_nxt[nbr] = modified_new;
+        finished[0] = false ;
+      }
     }
   }
+}
 
-  #endif
+#endif
