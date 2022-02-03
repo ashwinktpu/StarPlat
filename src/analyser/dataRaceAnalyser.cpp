a7709136f2fa1c38619cd080a462ae87cd67ce42
naveenls <naveenrocks62@gmail.com>
2022-02-03 19:08:14 +0530

    sssp_analysis

--- src/analyser/dataRaceAnalyser.cpp
+++ src/analyser/dataRaceAnalyser.cpp
@@ -1,59 +1,64 @@
 #include "dataRaceAnalyser.h"
+#include "../ast/ASTHelper.cpp"
 
-enum codeStructure {INVALID_STUCTURE, MIN_PROP_UPDATE, PROP_UPDATE};
+enum codeStructure
+{
+    INVALID_STUCTURE,
+    MIN_PROP_UPDATE,
+    PROP_UPDATE
+};
 
-bool checkIdEqual(Identifier* id1, Identifier* id2)
+bool checkIdEqual(Identifier *id1, Identifier *id2)
 {
     return (strcmp(id1->getIdentifier(), id2->getIdentifier()) == 0);
 }
 
-bool checkExprEqual(Expression* expr1, Expression* expr2)
+bool checkExprEqual(Expression *expr1, Expression *expr2)
 {
     //to check whether two expressions are equal
     return true;
 }
 
-int getStructure(statement* stmt, Identifier* itr1, Identifier* itr2)
+int getStructure(statement *stmt, Identifier *itr1, Identifier *itr2)
 {
-    if(stmt->getTypeofNode() == NODE_IFSTMT)
+    if (stmt->getTypeofNode() == NODE_IFSTMT)
     {
-        ifStmt* currStmt = (ifStmt*) stmt;
-        Expression* cond = currStmt->getCondition();
-        if((currStmt->getElseBody() == nullptr) && cond->isRelational() && (cond->getOperatorType() == OPERATOR_GT))
+        ifStmt *currStmt = (ifStmt *)stmt;
+        Expression *cond = currStmt->getCondition();
+        if ((currStmt->getElseBody() == nullptr) && cond->isRelational() && (cond->getOperatorType() == OPERATOR_GT))
         {
-            Expression* lExpr = cond->getLeft();
-            if(lExpr->isPropIdExpr() && checkIdEqual(lExpr->getPropId()->getIdentifier1(), id2))
+            Expression *lExpr = cond->getLeft();
+            if (lExpr->isPropIdExpr() && checkIdEqual(lExpr->getPropId()->getIdentifier1(), id2))
             {
-                blockStatement* ifBody = (blockStatement*) currStmt->getIfBody();
-                statement* reqStmt = nullptr;
+                blockStatement *ifBody = (blockStatement *)currStmt->getIfBody();
+                statement *reqStmt = nullptr;
                 int cntValid = 0;
 
-                for(statement* stmt: ifBody->returnStatements())
+                for (statement *stmt : ifBody->returnStatements())
                 {
-                    if(stmt->getTypeofNode() == NODE_ASSIGN)
+                    if (stmt->getTypeofNode() == NODE_ASSIGN)
                     {
-                        assignment* assgn = (assignment*) stmt;
-                        if(assgn->lhs_isProp())
+                        assignment *assgn = (assignment *)stmt;
+                        if (assgn->lhs_isProp())
                         {
-                            PropAccess* propId = assgn->getPropId();
-                            PropAccess* ifPropId = lExpr->getPropId();
+                            PropAccess *propId = assgn->getPropId();
+                            PropAccess *ifPropId = lExpr->getPropId();
 
-                            if(checkIdEqual(ifPropId->getIdentifier1(), propId->getIdentifier1()) 
-                                && checkIdEqual(ifPropId->getIdentifier2(), propId->getIdentifier2()))
+                            if (checkIdEqual(ifPropId->getIdentifier1(), propId->getIdentifier1()) && checkIdEqual(ifPropId->getIdentifier2(), propId->getIdentifier2()))
                             {
-                                if((reqStmt == nullptr) && checkExprEqual(cond->getRight(), assgn->getExpr()))
+                                if ((reqStmt == nullptr) && checkExprEqual(cond->getRight(), assgn->getExpr()))
                                 {
                                     reqStmt = stmt;
                                     cntValid++;
                                 }
                             }
-                            else if(assgn->getExpr()->isLiteral())
+                            else if (assgn->getExpr()->isLiteral())
                                 cntValid++;
                         }
                     }
                 }
 
-                if(cntValid == (ifBody->returnStatements()).size())
+                if (cntValid == (ifBody->returnStatements()).size())
                     return MIN_PROP_UPDATE;
             }
         }
@@ -89,12 +94,47 @@ void ASTAnalyser::analyseForAll(forallStmt *stmt)
                     if (argVal->isIdentifierExpr() &&
                         checkIdEqual(argVal->getId(), itr))
                     {
-                        blockStatement* forBody = (blockStatement*) forStmt->getBody();
-                        for(statement* istmt: forBody->returnStatements())
+                        blockStatement *forBody = (blockStatement *)forStmt->getBody();
+                        for (statement *istmt : forBody->returnStatements())
                         {
-                            if(getStructure(istmt, itr, itr1) == MIN_PROP_UPDATE)
+                            if (getStructure(istmt, itr, itr1) == MIN_PROP_UPDATE)
                             {
-                                reductionCallStmt* modifiedStmt = 
+                                ifStmt *currStmt = (ifStmt *)istmt;
+                                Expression *cond = currStmt->getCondition();
+
+                                list<ASTNode *> leftList;
+                                reductionCall *reductionCallNode;
+                                list<ASTNode *> rightList;
+
+                                leftList.push_back(cond->getLeft()->getPropId());
+
+                                argument *a1 = new argument();
+                                a1->setExpression(cond->getLeft());
+                                a1->setExpressionFlag();
+
+                                argument *a2 = new argument();
+                                a2->setExpression(cond->getRight());
+                                a2->setExpressionFlag();
+
+                                list<argument *> argList;
+                                argList.push_back(a1);
+                                argList.push_back(a2);
+
+                                reductionCallNode = Util::createNodeforReductionCall(REDUCE_MIN, argList);
+
+                                for(statement* ifBodyStmt: currStmt->getIfBody())
+                                {
+                                    assignment *assgn = (assignment *)stmt;
+                                    PropAccess *propId = assgn->getPropId();
+
+                                    if(assgn->getExpr()->isLiteral())
+                                    {
+                                        leftList.push_back(propId);
+                                        rightList.push_back(assgn->getExpr());
+                                    }
+                                }
+                            
+                                reductionCallStmt* modifiedStmt = Util::createNodeForReductionStmtList(leftList, reductionCallNode, rightList);
                             }
                         }
                     }
