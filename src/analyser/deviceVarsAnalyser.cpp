dea1680491d69f872777c6f40aa7ca1ada98658c
naveenls <naveenrocks62@gmail.com>
2022-02-23 18:01:20 +0530

    device vars analyser, analysing and printing of ASTNodes

--- src/analyser/deviceVarsAnalyser.cpp
+++ src/analyser/deviceVarsAnalyser.cpp
@@ -1,7 +1,9 @@
 #include "deviceVarsAnalyser.h"
 #include <string.h>
 #include <unordered_map>
+#include "analyserUtil.hpp"
 
+/*
 usedVariables deviceVarsAnalyser::getVarsWhile(whileStmt *stmt)
 {
   usedVariables currVars = getVarsExpr(stmt->getCondition());
@@ -15,30 +17,63 @@ usedVariables deviceVarsAnalyser::getVarsDoWhile(dowhileStmt *stmt)
   currVars.merge(getVarsStatement(stmt->getBody()));
 
   return currVars;
+}*/
+
+usedVariables seperatePropAccess(usedVariables inp)
+{
+  list<PropAccess*> ReadPropId = inp.getPropAcess(READ);
+  list<PropAccess*> WritePropId = inp.getPropAcess(WRITE);
+
+  usedVariables out;
+
+  for(Identifier* iden: inp.getVariables(READ)){
+    out.addVariable(iden, READ);
+  }
+
+  for(Identifier* iden: inp.getVariables(WRITE)){
+    out.addVariable(iden, WRITE);
+  }
+
+  for(PropAccess* prop: inp.getPropAcess(READ)){
+    out.addVariable(prop->getIdentifier1(), READ);
+    out.addVariable(prop->getIdentifier2(), READ);
+  }
+
+  for(PropAccess* prop: inp.getPropAcess(WRITE)){
+    out.addVariable(prop->getIdentifier1(), READ);
+    out.addVariable(prop->getIdentifier2(), WRITE);
+  }
+
+  return out;
 }
+
 usedVariables deviceVarsAnalyser::getVarsAssignment(assignment *stmt)
 {
   usedVariables currVars;
   if (stmt->lhs_isProp())
   {
     PropAccess *propId = stmt->getPropId();
-    currVars.addPropAccess(propId, WRITE);
+    currVars.addPropAccess(propId->getIdentifier1(), READ);
+    currVars.addPropAccess(propId->getIdentifier2(), WRITE);
   }
   else if (stmt->lhs_isIdentifier())
     currVars.addVariable(stmt->getId(), WRITE);
 
-  currVars.merge(getVarsExpr(stmt->getExpr()));
+  usedVariables exprVars = seperatePropAccess(getVarsExpr(stmt->getExpr));
+  currVars.merge(exprVars);
   return currVars;
 }
+
 usedVariables deviceVarsAnalyser::getVarsIf(ifStmt *stmt)
 {
-  usedVariables currVars = getVarsExpr(stmt->getCondition());
+  usedVariables currVars = seperatePropAccess(getVarsExpr(stmt->getCondition()));
   currVars.merge(getVarsStatement(stmt->getIfBody()));
   if (stmt->getElseBody() != NULL)
     currVars.merge(getVarsStatement(stmt->getElseBody()));
 
   return currVars;
 }
+/*
 usedVariables deviceVarsAnalyser::getVarsFixedPoint(fixedPointStmt *stmt)
 {
   usedVariables currVars = getVarsExpr(stmt->getDependentProp());
@@ -51,7 +86,7 @@ usedVariables deviceVarsAnalyser::getVarsReduction(reductionCallStmt *stmt)
 {
   usedVariables currVars;
   return currVars;
-}
+}*/
 
 usedVariables deviceVarsAnalyser::getVarsUnary(unary_stmt *stmt)
 {
@@ -62,11 +97,15 @@ usedVariables deviceVarsAnalyser::getVarsUnary(unary_stmt *stmt)
   if (varExpr->isIdentifierExpr())
     currUsed.addVariable(varExpr->getId(), READ_WRITE);
   else if (varExpr->isPropIdExpr())
-    currUsed.addPropAccess(varExpr->getPropId(), READ_WRITE);
+  {
+    currUsed.addVariable(varExpr->getPropId()->getIdentifier1(), READ);
+    currUsed.addVariable(varExpr->getPropId()->getIdentifier2(), WRITE);
+  }
 
   return currUsed;
 }
 
+/*
 usedVariables deviceVarsAnalyser::getVarsBFS(iterateBFS *stmt)
 {
   usedVariables currVars = getVarsStatement(stmt->getBody());
@@ -79,32 +118,51 @@ usedVariables deviceVarsAnalyser::getVarsBFS(iterateBFS *stmt)
   }
 
   return currVars;
-}
+}*/
 
-usedVariables deviceVarsAnalyser::getVarsFor(forallStmt *stmt)
+usedVariables deviceVarsAnalyser::getVarsForAll(forallStmt *stmt)
 {
-  usedVariables currVars;
+  usedVariables currVars = getVarsStatement(stmt->getBody());
+  currVars.removeVariable(stmt->getIterator(), READ_WRITE);
+
+  if(stmt->hasFilterExpr())
+    currVars.merge(seperatePropAccess(getVarsExpr(stmt->getfilterExpr())));
+  
   return currVars;
 }
 
 usedVariables deviceVarsAnalyser::getVarsBlock(blockStatement *blockStmt)
 {
   list<statement *> stmtList = blockStmt->returnStatements();
-  usedVariables currVars;
   list<Identifier *> declVars;
 
+  usedVariables currVars;
   for (statement *stmt : stmtList)
   {
     if (stmt->getTypeofNode() == NODE_DECL)
     {
       declaration *decl = (declaration *)stmt;
       declVars.push_back(decl->getdeclId());
+
+      if(decl->isInitialized())
+      {
+        usedVariables exprVars = getVarsExpr(decl->getExpressionAssigned());
+        exprVars = seperatePropAccess(exprVars);
+
+        for(Identifier* dVars: declVars){
+          exprVars.removeVariable(dVars, READ_WRITE);
+        }
+        currVars.addVariable(exprVars);
+      }
     }
     else
     {
-      usedVariables vars = getVarsStatement(stmt);
-      vars.removeVariable(declVars);
-      currVars.merge(vars);
+      usedVariables stmtVars = getVarsStatement(stmt);
+      for(Identifier* dVars: declVars){
+        stmtVars.removeVariable(dVars, READ_WRITE);
+      }
+
+      currVars.merge(stmtVars);
     }
   }
 
@@ -116,7 +174,22 @@ usedVariables deviceVarsAnalyser::getVarsStatement(statement *stmt)
   switch (stmt->getTypeofNode())
   {
   case NODE_BLOCKSTMT:
-    return getVarsBlock(stmt);
+    return getVarsBlock((blockStatement*) stmt);
+
+  case NODE_ASSIGN:
+    return getVarsAssignment((assignment*) stmt);
+
+  case NODE_UNARYSTMT:
+    return getVarsUnary((unary_stmt*) stmt);
+
+  case NODE_IFSTMT:
+    return getVarsIf((ifStmt *)stmt);
+
+  /*case NODE_ITRBFS:
+    return getVarsBFS((iterateBFS *)stmt);
+
+  case NODE_REDUCTIONCALLSTMT:
+    return getVarsReduction((reductionCallStmt *)stmt);
 
   case NODE_WHILESTMT:
     return getVarsWhile((whileStmt *)stmt);
@@ -125,113 +198,92 @@ usedVariables deviceVarsAnalyser::getVarsStatement(statement *stmt)
     return getVarsDoWhile((dowhileStmt *)stmt);
 
   case NODE_FIXEDPTSTMT:
-    return getVarsFixedPoint((fixedPointStmt *)stmt);
-
-  case NODE_IFSTMT:
-    return getVarsIf((ifStmt *)stmt);
-
-  case NODE_ITRBFS:
-    return getVarsBFS((iterateBFS *)stmt);
-
-  case NODE_REDUCTIONCALLSTMT:
-    return getVarsReduction((reductionCallStmt *)stmt);
+    return getVarsFixedPoint((fixedPointStmt *)stmt);*/
   }
 
   return usedVariables();
 }
 
-void deviceVarsAnalyser::analyseForAll(forallStmt *stmt)
+//Statement Analyser
+//Current assuming filter expression gets evaluated in GPU
+
+void deviceVarsAnalyser::analyseForAll(forallStmt *stmt, lattice& inp)
 {
-  Identifier *itr = stmt->getIterator();
-  Identifier *graph = stmt->getSourceGraph();
+  ASTNodeWrap* wrapNode = getWrapNode(stmt, inp);
+  wrapNode->inMap = inp;
 
-  usedVariables vars = getVarsStatment(stmt->getBody());
-}
+  lattice outMap = inp;
 
-void deviceVarsAnalyser::analyseStatement(statement *stmt)
-{
-  switch (stmt->getTypeofNode())
-  {
-  case NODE_BLOCKSTMT:
-    for (statement *stmtB : ((blockStatement *)stmt)->returnStatements())
-      analyseStatement(stmtB);
-    break;
-
-  case NODE_WHILESTMT:
-    analyseStatement(((whileStmt *)stmt)->getBody());
-    break;
-
-  case NODE_DOWHILESTMT:
-    analyseStatement(((dowhileStmt *)stmt)->getBody());
-    break;
-
-  case NODE_FIXEDPTSTMT:
-    analyseStatement(((fixedPointStmt *)stmt)->getBody());
-    break;
-
-  case NODE_IFSTMT:
-    statement *ifBody = ((ifStmt *)stmt)->getIfBody();
-    statement *elseBody = ((ifStmt *)stmt)->getElseBody();
-    if (ifBody != NULL)
-      analyseStatement(ifBody);
-    if (elseBody != NULL)
-      analyseStatement(elseBody);
-    break;
-
-  case NODE_ITRBFS:
-    analyseStatement(((iterateBFS *)stmt)->getBody());
-    iterateReverseBFS *revBFS = ((iterateBFS *)stmt)->getRBFS();
-    if (revBFS != NULL)
-      analyseStatement(revBFS->getBody());
-    break;
-
-  case NODE_FORALLSTMT:
-    analyseForAll((forallStmt *)stmt);
-    break;
+  usedVariables vars = getVarsForAll(stmt);
+  for(Identifier* iden: vars.getVariables(READ)){
+    outMap.meet(iden, lattice::GPU_READ);
   }
+  for(Identifier* iden: vars.getVariables(READ)){
+    outMap.meet(iden, lattice::GPU_WRITE);
+  }
+
+  wrapNode->outMap = outMap;
+  return outMap;
 }
 
 lattice deviceVarsAnalyser::analyseDeclaration(declaration *stmt, lattice &inMap)
 {
+  ASTNodeWrap* wrapNode = getWrapNode(stmt, inMap);
+  
+  lattice outMap = inMap;
+  outMap.addVariable(stmt->getdeclId(), lattice::CPU_ONLY);
+
+  wrapNode->outMap = outMap;
+  return outMap;
 }
 
 lattice deviceVarsAnalyser::analyseAssignment(assignment *stmt, lattice &inMap)
 {
-}
+  ASTNodeWrap* wrapNode = getWrapNode(stmt, inMap);
+  usedVariables usedVars = getVarsAssignment(stmt);
 
-lattice deviceVarsAnalyser::analyseWhile(whileStmt *stmt, lattice &inMap)
-{
-}
+  wrapNode->inMap = inMap;
 
-lattice deviceVarsAnalyser::analyseDoWhile(dowhileStmt *stmt, lattice &inMap)
-{
-}
-
-lattice deviceVarsAnalyser::analyseFixedPoint(fixedPointStmt *stmt, lattice &inMap)
-{
+  lattice outMap = wrapNode->inMap;
+  for(Identifier* iden: usedVars.getVariables(READ)){
+    outMap.meet(iden, lattice::CPU_READ);  
+  }
+  for(Identifier* iden: usedVars.getVariables(WRITE)){
+    outMap.meet(iden, lattice::CPU_WRITE);
+  }
 }
 
 lattice deviceVarsAnalyser::analyseIfElse(ifStmt *stmt, lattice &inMap)
 {
   Expression* cond = stmt->getCondition();
+  ASTNodeWrap* wrapNode = getWrapNode(cond);
+  wrapNode->inMap = inMap;
+
+  lattice condOut = inMap;
+  usedVariables exprVars = seperatePropAccess(getVarsExpr(cond));
+
+  for(Identifier* iden: exprVars.getVariables(READ)){
+    condOut.meet(iden, lattice::CPU_READ);
+  }
+  for(Identifier* iden: exprVars.getVariables(WRITE)){
+    condOut.meet(iden, lattice::CPU_WRITE);
+  }
+
+  wrapNode->outMap = condOut;
+
+  lattice ifOut = analyseStatement(stmt->getIfBody(), condOut);
+  ASTNodeWrap* stmtWrap;
+
+  if(stmt->getElseBody() != nullptr)
+  {
+    lattice elseOut = analyseStatement(stmt->getElseBody(), condOut);
+    stmtWrap = getWrapNode(stmt, ifOut ^ elseOut);
+  }
+  else
+    stmtWrap = getWrapNode(stmt, ifOut ^ condOut);
   
-}
-
-lattice deviceVarsAnalyser::analyseItrBFS(iterateBFS *stmt, lattice &inMap)
-{
-}
-
-lattice deviceVarsAnalyser::analyseFor(forallStmt *stmt, lattice &inMap)
-{
-}
-
-lattice deviceVarsAnalyser::analyseForAll(forallStmt *stmt, lattice &inMap)
-{
-}
-
-lattice deviceVarsAnalyser::analyseReduction(reductionCallStmt *stmt, lattice &inMap)
-{
-
+  stmtWrap->outMap = stmtWrap->inMap;
+  return stmtWrap->outMap;
 }
 
 lattice deviceVarsAnalyser::analyseUnary(unary_stmt *stmt, lattice &inMap)
@@ -243,18 +295,44 @@ lattice deviceVarsAnalyser::analyseUnary(unary_stmt *stmt, lattice &inMap)
 
   lattice outMap = inMap;
   if(unaryVar->isIdentifierExpr()){
-    outMap.meet(unaryVar->getId(), lattice::CPU_ONLY);
+    outMap.meet(unaryVar->getId(), lattice::CPU_WRITE);
   }
   else if(unaryVar->isPropIdExpr())
   {
     PropAccess* propId = unaryVar->getPropId();
-    outMap.meet(propId->getIdentifier1(), lattice::CPU_GPU_SHARED);
-    outMap.meet(propId->getIdentifier2(), lattice::CPU_ONLY);
+    outMap.meet(propId->getIdentifier1(), lattice::CPU_READ);
+    outMap.meet(propId->getIdentifier2(), lattice::CPU_WRITE);
   }
   wrapNode->outMap = outMap;
 
   return outMap;
 }
+
+/*lattice deviceVarsAnalyser::analyseWhile(whileStmt *stmt, lattice &inMap)
+{
+}
+
+lattice deviceVarsAnalyser::analyseDoWhile(dowhileStmt *stmt, lattice &inMap)
+{
+}
+
+lattice deviceVarsAnalyser::analyseFixedPoint(fixedPointStmt *stmt, lattice &inMap)
+{
+}
+
+lattice deviceVarsAnalyser::analyseItrBFS(iterateBFS *stmt, lattice &inMap)
+{
+}
+
+lattice deviceVarsAnalyser::analyseFor(forallStmt *stmt, lattice &inMap)
+{
+}
+
+lattice deviceVarsAnalyser::analyseReduction(reductionCallStmt *stmt, lattice &inMap)
+{
+
+}*/
+
 
 lattice deviceVarsAnalyser::analyseStatement(statement *stmt, lattice &inMap)
 {
@@ -264,23 +342,25 @@ lattice deviceVarsAnalyser::analyseStatement(statement *stmt, lattice &inMap)
     return analyseDeclaration((declaration *)stmt, inMap);
   case NODE_ASSIGN:
     return analyseAssignment((assignment *)stmt, inMap);
-  case NODE_FIXEDPTSTMT:
-    return analyseFixedPoint((fixedPointStmt *)stmt, inMap);
   case NODE_FORALLSTMT:
-    {
-      forallStmt* forStmt = (forallStmt*) stmt;
+  {
+    forallStmt* forStmt = (forallStmt*) stmt;
 
-      if(forStmt->isForall())
-        return analyseForAll((forallStmt*)stmt, inMap);
-      else
-        return analyseFor((forallStmt*)stmt, inMap);
-    }
+    if(forStmt->isForall())
+      return analyseForAll((forallStmt*)stmt, inMap);
+    /*else
+      return analyseFor((forallStmt*)stmt, inMap);*/
+  }
   case NODE_BLOCKSTMT:
     return analyseBlock((blockStatement *)stmt, inMap);
-  case NODE_WHILESTMT:
-    return analyseWhile((whileStmt *)stmt, inMap);
+  case NODE_UNARYSTMT:
+    return analyseUnary((unary_stmt *)stmt, inMap);
   case NODE_IFSTMT:
     return analyseIfElse((ifStmt *)stmt, inMap);
+  
+  /*
+  case NODE_WHILESTMT:
+    return analyseWhile((whileStmt *)stmt, inMap);
   case NODE_PROCCALLSTMT:
     // TODO : Add proc call statment
     return inMap;
@@ -290,8 +370,8 @@ lattice deviceVarsAnalyser::analyseStatement(statement *stmt, lattice &inMap)
     return analyseItrBFS((iterateBFS *)stmt, inMap);
   case NODE_DOWHILESTMT:
     return analyseDoWhile((dowhileStmt *)stmt, inMap);
-  case NODE_UNARYSTMT:
-    return analyseUnary((unary_stmt *)stmt, inMap);
+  case NODE_FIXEDPTSTMT:
+    return analyseFixedPoint((fixedPointStmt *)stmt, inMap);*/
   }
 
   return inMap;
@@ -313,18 +393,121 @@ lattice deviceVarsAnalyser::analyseBlock(blockStatement *stmt, lattice &inMap)
     if (bstmt->getTypeofNode() == NODE_DECL)
       declaredVars.push_back(((declaration *)bstmt)->getdeclId());
   }
-  wrapNode->outMap = lastOut;
-
   for (Identifier *iden : declaredVars)
     lastOut.removeVariable(iden);
 
+  wrapNode->outMap = lastOut;
   return lastOut;
 }
+
+void printTabs(int tabSpace)
+{
+  while(tabSpace--)
+    cout<<'\t';
+}
+
+void deviceVarsAnalyser::printBlock(blockStatement *stmt, int tabSpace)
+{
+  printTabs(tabSpace);
+  cout<<"Block Begin\n";
+  tabSpace++;
+  for (statement *bstmt : stmt->returnStatements()){
+    printStatement(bstmt);
+  }
+  tabSpace--;
+  printTabs(tabSpace);
+  cout<<"Block End\n";
+}
+
+
+void deviceVarsAnalyser::printDeclaration(declaration *stmt, int tabSpace)
+{
+  ASTNodeWrap* wrapNode = getWrapNode(stmt);
+  printTabs(tabSpace);
+  cout<<"Declaration: ";
+  (wrapNode->outMap).print();
+}
+
+void deviceVarsAnalyser::printAssignment(assignment *stmt, int tabSpace)
+{
+  ASTNodeWrap* wrapNode = getWrapNode(stmt);
+  printTabs(tabSpace);
+  cout<<"Assignment: ";
+  (wrapNode->outMap).print();
+}
+
+void deviceVarsAnalyser::printIfElse(ifStmt *stmt, int tabSpace)
+{
+  printTabs(tabSpace);
+  cout<<"If Statement\n";
+
+  tabSpace++;
+  printTabs(tabSpace);
+  cout<<"Condition: ";
+
+  Expression* cond = stmt->getCondition();
+  ASTNodeWrap* wrapNode = getWrapNode(cond);
+  (wrapNode->outMap).print();
+
+  cout<<"If Body\n";
+  tabSpace++;
+  printStatement(stmt->getIfBody(), tabSpace);
+  tabSpace--;
+
+  if(stmt->getElseBody() != nullptr)
+  {
+    cout<<"Else Body\n";
+    tabSpace++;
+    printStatement(stmt->getElseBody(), tabSpace);
+    tabSpace--;
+  }
+  
+  printTabs();
+  (stmtWrap->outMap).print();
+  tabSpace--;
+}
+
+void deviceVarsAnalyser::printUnary(unary_stmt *stmt)
+{
+  ASTNodeWrap* wrapNode = getWrapNode(stmt);
+  printTabs(tabSpace);
+  cout<<"Unary statment: ";
+  (wrapNode->outMap).print();
+}
+
+void deviceVarsAnalyser::printStatement(statement *stmt, int tabSpace)
+{
+  switch (stmt->getTypeofNode())
+  {
+  case NODE_DECL:
+    printDeclaration((declaration *)stmt, tabSpace);
+  case NODE_ASSIGN:
+    printAssignment((assignment *)stmt, tabSpace);
+  case NODE_FORALLSTMT:
+  {
+    forallStmt* forStmt = (forallStmt*) stmt;
+
+    if(forStmt->isForall())
+      printForAll((forallStmt*)stmt, tabSpace);
+  }
+  case NODE_BLOCKSTMT:
+    printBlock((blockStatement *)stmt, tabSpace);
+  case NODE_UNARYSTMT:
+    printUnary((unary_stmt *)stmt, tabSpace);
+  case NODE_IFSTMT:
+    printIfElse((ifStmt *)stmt, tabSpace);
+  }
+
+}
+
+
 
 void deviceVarsAnalyser::analyseFunc(ASTNode *proc)
 {
   Function *func = (Function *)proc;
-  analyseStatement(func->getBlockStatement());
+
+  lattice inpLattice;
+  analyseStatement(func->getBlockStatement(), inpLattice);
   return;
 }
 
