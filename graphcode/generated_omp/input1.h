5b622195e3186666a3252b231accfd8d400b82c5
naveenls <naveenrocks62@gmail.com>
2022-03-26 17:26:28 +0530

    generated output

---
+++ graphcode/generated_omp/input1.h
@@ -0,0 +1,12 @@
+#ifndef GENCPP_INPUT1_H
+#define GENCPP_INPUT1_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<atomic>
+#include<omp.h>
+#include"../graph.hpp"
+
+void test(graph& g,int src);
+
+#endif
