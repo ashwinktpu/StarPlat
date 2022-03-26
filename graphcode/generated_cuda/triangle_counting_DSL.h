abd7c243c6d6382574b7f6cfd39baa2a71700f56
Rajesh Pandian M <mrpraj@gmail.com>
2022-03-26 16:05:24 +0530

    SSSP: fixed pt stmts2

--- graphcode/generated_cuda/triangle_counting_DSL.h
+++ graphcode/generated_cuda/triangle_counting_DSL.h
@@ -1,3 +1,4 @@
+// FOR BC: nvcc bc_dsl_v2.cu -arch=sm_60 -std=c++14 -rdc=true # HW must support CC 6.0+ Pascal or after
 #ifndef GENCPP_TRIANGLE_COUNTING_DSL_H
 #define GENCPP_TRIANGLE_COUNTING_DSL_H
 #include <stdio.h>
@@ -12,7 +13,7 @@ void Compute_TC(graph& g);
 
 __device__ long triangle_count = 0; // DEVICE ASSTMENT in .h
 
-__global__ void Compute_TC_kernel(int V, int E, int* d_meta, int* d_data, int* d_weight ,graph& g){ // BEGIN FUN via ADDKERNEL
+__global__ void Compute_TC_kernel(int V, int E, int* d_meta, int* d_data, int* d_weight ,graph& g){ // BEGIN KER FUN via ADDKERNEL
   unsigned v = blockIdx.x * blockDim.x + threadIdx.x;
   if(v >= V) return;
   for (int edge = d_meta[v]; edge < d_meta[v+1]; edge++) { // FOR NBR ITR 
@@ -25,9 +26,9 @@ __global__ void Compute_TC_kernel(int V, int E, int* d_meta, int* d_data, int* d
             atomicAdd(& triangle_count, (long)1);
           } // if filter end
         } // if filter end
-      } // FOR END
+      } //  end FOR NBR ITR. TMP FIX!
     } // if filter end
-  } // FOR END
-} // end FUNC
+  } //  end FOR NBR ITR. TMP FIX!
+} // end KER FUNC
 
 #endif
