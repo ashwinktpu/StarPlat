93d3e5a000a9140992a356b584f0253653c70e8f
Rajesh Pandian M <mrpraj@gmail.com>
2022-03-26 13:22:02 +0530

    SSSP:Fixed Filter expr. Thanks Nibedita!

--- graphcode/generated_cuda/sssp_dslV2.h
+++ graphcode/generated_cuda/sssp_dslV2.h
@@ -16,18 +16,20 @@ __device__ bool finished = false; // DEVICE ASSTMENT in .h
 __global__ void Compute_SSSP_kernel(int V, int E, int* d_meta, int* d_data, int* d_weight ,graph& g, int* d_dist, int src){ // BEGIN FUN via ADDKERNEL
   unsigned v = blockIdx.x * blockDim.x + threadIdx.x;
   if(v >= V) return;
-  for (int edge = d_meta[v]; edge < d_meta[v+1]; edge++) { // FOR NBR ITR 
-    int nbr = d_data[edge];
-    int e = edge;
-     int dist_new = d_dist[v] + d_weight[e];
-    bool modified_new = true;
-    if(dist[v]!= MAX_VAL && d_dist[nbr] > dist_new)
-    {
-      atomicMin(&d_dist[nbr],dist_new);
-      modified_nxt[nbr] = modified_new;
-      finished = false ;
-    }
-  } //  end FOR NBR ITR. TMP FIX!
+  if (d_modified[v] == true){ // if filter begin 
+    for (int edge = d_meta[v]; edge < d_meta[v+1]; edge++) { // FOR NBR ITR 
+      int nbr = d_data[edge];
+      int e = edge;
+       int dist_new = d_dist[v] + d_weight[e];
+      bool modified_new = true;
+      if(d_dist[v]!= MAX_VAL && d_dist[nbr] > dist_new)
+      {
+        atomicMin(&d_dist[nbr],dist_new);
+        modified_nxt[nbr] = modified_new;
+        finished = false ;
+      }
+    } //  end FOR NBR ITR. TMP FIX!
+  } // if filter end
 } // end FUNC
 
 #endif
