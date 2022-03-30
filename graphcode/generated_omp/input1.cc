898e8a1afa777d6cbb2b283b90bff64c77871bd3
naveenls <naveenrocks62@gmail.com>
2022-03-30 17:02:12 +0530

    bin

--- graphcode/generated_omp/input1.cc
+++ graphcode/generated_omp/input1.cc
@@ -2,22 +2,12 @@
 
 void test(graph& g,int src)
 {
-  int y = 0 ;
   int x = 0 ;
+  x = 100;
+  int y = 0 ;
+  y = 80;
+  y = x + y;
   int* prop=new int[g.num_nodes()];
-  if (y )
-    {
-    #pragma omp parallel for
-    for (int v = 0; v < g.num_nodes(); v ++) 
-    {
-      int t = x + 1;
-      prop[v] = 100;
-    }
-  }
-  else
-  {
-    prop[src] = 100;
-  }
-  y = x;
+  prop[src] = x;
 
 }
---
+++ graphcode/generated_omp/input2.h
@@ -0,0 +1,12 @@
+#ifndef GENCPP_INPUT2_H
+#define GENCPP_INPUT2_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<atomic>
+#include<omp.h>
+#include"../graph.hpp"
+
+void test(graph& g,int src);
+
+#endif
