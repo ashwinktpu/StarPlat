57e808f1a78aec464815bc69bf88270bd1225225
Vineeth-Kada <cs19b080@smail.iitm.ac.in>
2022-09-10 12:41:33 +0530

    debug

--- graphcode/generated_cuda/MST_V1.cu
+++ graphcode/generated_cuda/MST_V1.cu
@@ -213,6 +213,7 @@ int main(int argc, char** argv) {
     char* inp = argv[1];
     bool isWeighted = atoi(argv[2]) ? true : false;
     printf("Taking input from: %s\n", inp);
+    printf("Weighted: %d\n", isWeighted);
     graph g(inp);
     g.parseGraph(isWeighted);
     Boruvka(g);
