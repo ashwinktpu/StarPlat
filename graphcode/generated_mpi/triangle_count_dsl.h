403397ffd6bc5c3859486fbc52437816643e0b1d
ebenezerrajadurai <ebenezerrajadurai5@gmail.com>
2022-09-19 22:05:07 +0530

    TC updated

--- graphcode/generated_mpi/triangle_count_dsl.h
+++ graphcode/generated_mpi/triangle_count_dsl.h
@@ -19,5 +19,47 @@ using namespace std;
 namespace mpi = boost::mpi;
 
 void Compute_TC(graph g);
+bool check_if_nbr(int s, int d, int* indexofNodes,int* edgeList, int startv)
+{
+    int startEdge=indexofNodes[s - startv];
+    int endEdge=indexofNodes[s - startv + 1]-1;
+    if(edgeList[startEdge]==d)
+      return true;
+    if(edgeList[endEdge]==d)
+      return true;
+    int mid = (startEdge+endEdge)/2;
+    while(startEdge<=endEdge)
+    {
+        if(edgeList[mid]==d)
+          return true;
+        if(d<edgeList[mid])
+          endEdge=mid-1;
+        else
+          startEdge=mid+1;
+        mid = (startEdge+endEdge)/2;
+    }
+    return false;
+}
+bool check_if_nbr_new(int s, int d, vector <int>& s_List, vector <int>& d_List)
+{
+    int startEdge=0;
+    int endEdge=s_List.size() - 1;
+    if(s_List[startEdge]==d)
+      return true;
+    if(s_List[endEdge]==d)
+      return true;
+    int mid = (startEdge+endEdge)/2;
+    while(startEdge<=endEdge)
+    {
+        if(s_List[mid]==d)
+          return true;
+        if(d<s_List[mid])
+          endEdge=mid-1;
+        else
+          startEdge=mid+1;
+        mid = (startEdge+endEdge)/2;
+    }
+    return false;
+}
 
 #endif
