0a32c58d1abefd3604fdf8db6c4c76f3b988e8d0
Vineeth-Kada <cs19b080@smail.iitm.ac.in>
2022-10-09 09:04:02 +0530

    Aqua Sync

--- graphcode/run.sh
+++ graphcode/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -e errorLogMST.err
-#PBS -o logMST.log
+#PBS -e errorLogAPFB.err
+#PBS -o logAPFB.log
 #PBS -q rupesh_gpuq
 #PBS -l select=1:ncpus=1:ngpus=1
 
@@ -14,19 +14,9 @@ cd $tempdir
 cp -R $PBS_O_WORKDIR/* . # PBS_O_WORKDIR is the directory from which the job was submitted
 module load cuda10.1
 module load gcc640
-nvcc -o "MST_V1_Manual".out "generated_cuda/MST_V1.cu"  -std=c++14  #-arch=sm_70
-time (./MST_V1_Manual.out $cleandir/GermanyRoadud.txt 0 > output_MST_V1_Manual.txt) # 0 - Unweighted Graph
-time (./MST_V1_Manual.out $cleandir/cleancom-orkutud.txt 0 >> output_MST_V1_Manual.txt) # 0 - Unweighted Graph
-time (./MST_V1_Manual.out $cleandir/livud.txt 0 >> output_MST_V1_Manual.txt) # 0 - Unweighted Graph
-time (./MST_V1_Manual.out $cleandir/rmatud.txt 0 >> output_MST_V1_Manual.txt) # 0 - Unweighted Graph
-time (./MST_V1_Manual.out $cleandir/u10m_80mud.txt 0 >> output_MST_V1_Manual.txt) # 0 - Unweighted Graph
-time (./MST_V1_Manual.out $cleandir/USAud.txt 0 >> output_MST_V1_Manual.txt) # 0 - Unweighted Graph
-time (./MST_V1_Manual.out $cleandir/livjournalud.txt 0 >> output_MST_V1_Manual.txt) # 0 - Unweighted Graph
-time (./MST_V1_Manual.out $cleandir/pokecud.txt 0 >> output_MST_V1_Manual.txt) # 0 - Unweighted Graph
-time (./MST_V1_Manual.out $cleandir/sinaweiboud.txt 0 >> output_MST_V1_Manual.txt) # 0 - Unweighted Graph
-time (./MST_V1_Manual.out $cleandir/twitud.txt 0 >> output_MST_V1_Manual.txt) # 0 - Unweighted Graph
-time (./MST_V1_Manual.out $cleandir/wikiud.txt 0 >> output_MST_V1_Manual.txt) # 0 - Unweighted Graph
-#./MST_V1_Manual.out input1.txt 0 > output_MST_V1_Manual.txt
+nvcc -o "APFB_V1".out "generated_cuda/APFB_V1_main.cu"  -std=c++14  #-arch=sm_70
+# time (./APFB_V1.out $cleandir/GermanyRoadud.txt 0 > output_APFB_V1.txt) # 0 - Unweighted Graph
+time (./APFB_V1.out bipartite1.txt 0 > output_APFB_V1.txt) # 0 - Unweighted Graph
 
 rm *.out
 # move everything from $tempdir to $PBS_O_WORKDIR
