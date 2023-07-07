6ceacc0466d3452a4019e31c3eb3a5e9f65cd830
Mathew Thomas <mathewthomas358@gmail.com>
2023-07-07 12:59:19 +0530

    Add sssp file for hip

---
+++ graphcode/manual_amdhip/sssp_dsl.cc
@@ -0,0 +1,32 @@
+/**
+ * Manual implementation for Single Source Shortest Path 
+ * Parallel Algorithm using AMD HIP Code. 
+ * 
+ * @author: cs22m056
+*/
+
+#include <hip/hip_runtime.h>
+#include <iostream>
+
+#define NL '\n'
+
+using std::cin;
+using std::cout;
+
+__global__
+void SSSP() {
+
+
+}
+
+void ComputeSSSP() {
+
+
+}
+
+int main(void) {
+
+    
+
+    return 0;
+}
