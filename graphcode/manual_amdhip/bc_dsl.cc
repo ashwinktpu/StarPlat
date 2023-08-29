b33609b94e2d004801a15212c346f1015da97097
Mathew Thomas <mathewthomas358@gmail.com>
2023-08-29 14:17:29 +0530

    Fix bc amd hip implementation, add misc and script folder

--- graphcode/manual_amdhip/bc_dsl.cc
+++ graphcode/manual_amdhip/bc_dsl.cc
@@ -12,18 +12,24 @@
 
 #include "../graph.hpp"
 
+int count = 0;
+__device__ int countd = 0;
+
 #define NL '\n'
+// ! EXTREMELY DANGEROUS MACROS BELOW
+#define debug_flag false
+#define DEBUG if(debug_flag) 
+#define hds hipDeviceSynchronize();
 
 using std::cin;
 using std::cout;
 using std::vector;
 
-
 __global__
 void PrintArray(double* array, long size) {
 
 	for(int i = 0; i < size; i++)
-		printf("%f ", array[i]);
+		printf("%1.0f ", array[i]);
 	printf("\n");
 }
 
@@ -51,26 +57,26 @@ void InitializeArrayLocation(T* array, T value, long location, long size) {
 
 __global__
 void BreadthFirstSearch(
-	int nv, int* d_meta, int* d_data, double* d_delta,
-	double* d_sigma, int* d_d, int* level, bool* finished
+	int nv, int* d_meta, int* d_data, double* d_sigma,
+	int* d_d, int* level, bool* finished
 ) {
 
 	unsigned v = blockIdx.x * blockDim.x + threadIdx.x;
 
-	// printf("v = %d dd = %d level = %d\n", v, d_d[v], *level);
 	if(v >= nv)
 		return;
 
+	DEBUG printf("Current Node: %d\n", v); 
+
 	if(d_d[v] == *level) {
 
-		for(int e = d_meta[v]; e < d_meta[v+1]; e++) {
+		for(int e = d_meta[v]; e < d_meta[v+1]; e++) { // ? Dynamic Parelelism
 
 			int w = d_data[e];
 
-			// printf("Current Node: %d Neighbour: %d\n", v, w);
-
 			if(d_d[w] < 0) {
 
+				// atomicCAS(&d_d[w], -1, *level + 1);
 				d_d[w] = *level + 1; //? Atomics required?
 				*finished = false;
 			}
@@ -88,10 +94,17 @@ void ReversePass(
 ) {
 
 	unsigned v = blockIdx.x * blockDim.x + threadIdx.x;
+
 	if(v >= nv)
 		return;
 
-	// auto grid = cooperative_groups::this_grid();
+	// auto grid = cooperative_groups::this_grid(); // TODO
+
+	// !
+	// ! POSSIBLE PROBLEM
+	// !
+	// ! Check the levels things. 
+	// !
 
 	if(d_d[v] == *level - 1) {
 
@@ -111,28 +124,26 @@ void ReversePass(
 }
 
 __global__
-void AddElements(int nv, double* a, double* b) {
+void AddElements(int nv, double* a, double* b, int* d_d, int* level, int src) {
 
 	unsigned v = blockIdx.x * blockDim.x + threadIdx.x;
 	if(v >= nv)
 		return;
 
-	a[v] += b[v];
+	if(d_d[v] == *level - 1 && v != src)
+		a[v] += b[v];
 }
 
-double* ComputeBetweennessCentrality(graph& g, vector<int>& sources) { // TODO: sources should be a set
+void ComputeBetweennessCentrality(graph& g, vector<int>& sources, double *h_bc) { // TODO: sources should be a set
 
 	unsigned V = g.num_nodes();
 	unsigned E = g.num_edges();
 
-	vector<double> bc(V);
-
 	printf("Nodes : %d\n", V);
 	printf("Edges : %d\n", E);
 
-	int *h_meta = (int *)malloc((V+1) * sizeof(int));
-	int *h_data = (int *)malloc((E) * sizeof(int));
-	int *h_weight = (int *)malloc((E) * sizeof(int));
+	int *h_meta = (int*) malloc(V * sizeof(int));
+	int *h_data = (int*) malloc(E * sizeof(int));
 
 	for(int i=0; i<= V; i++)
 		h_meta[i] = g.indexofNodes[i];
@@ -142,25 +153,25 @@ double* ComputeBetweennessCentrality(graph& g, vector<int>& sources) { // TODO: 
 
 	int* d_meta;
 	int* d_data;
+	double* d_bc;	
 
-	hipMalloc(&d_meta, sizeof(int) * (V + 1));
+	hipMalloc(&d_meta, sizeof(int) * V);
 	hipMalloc(&d_data, sizeof(int) * E);
-
-	hipMemcpy(d_meta, h_meta, sizeof(int) * (V + 1), hipMemcpyHostToDevice);
-	hipMemcpy(d_data, h_data, sizeof(int) * E, hipMemcpyHostToDevice);
-
-	double* d_bc;
-	double* h_bc = (double*) malloc(sizeof(double) * V);
 	hipMalloc(&d_bc, sizeof(double) * V);
 
-	// TODO: Temp launch config
+	hipMemcpy(d_meta, h_meta, sizeof(int) * V, hipMemcpyHostToDevice);
+	hipMemcpy(d_data, h_data, sizeof(int) * E, hipMemcpyHostToDevice);
 
-	int threads = 1024; //! TODO: try other confid
-	// int threads = V;
+	// ! TODO: Temp launch config
+
+	int threads = 1024; //! TODO: try other config
 	int blocks = ceil(V / 1024.0);
-	// int blocks = 1;
 
-	// TODO: BEGIN TIMING
+	hipEvent_t start, stop;
+	hipEventCreate(&start);
+	hipEventCreate(&stop);
+	float milliseconds = 0;
+	hipEventRecord(start,0);
 
 	InitializeArray<double><<<blocks, threads>>>(d_bc, 0.0, V);
 
@@ -178,24 +189,23 @@ double* ComputeBetweennessCentrality(graph& g, vector<int>& sources) { // TODO: 
 	int* level;
 	hipHostMalloc(&level, sizeof(int), 0);
 
-	for(int i: sources) {
+	for(int s: sources) {
 
 		InitializeArray<double><<<blocks, threads>>>(d_sigma, 0.0, V);
-		InitializeArray<double><<<blocks, threads>>>(d_delta, 0.0, V);
-		InitializeArray<int><<<blocks, threads>>>(d_d, -1, V);
-		InitializeArrayLocation<double><<<1, 1>>>(d_sigma, 1.0, i, V);
-		InitializeArrayLocation<int><<<1, 1>>>(d_d, 0, i, V);
+		InitializeArrayLocation<double><<<1, 1>>>(d_sigma, 1.0, s, V);
+		InitializeArray<int><<<blocks, threads>>>(d_d, -1, V); // ? Streams?
+		InitializeArrayLocation<int><<<1, 1>>>(d_d, 0, s, V);
 
 		*isAllNodesTraversed = false;
 		*level = 0;
 
-		hipDeviceSynchronize();
+		hipDeviceSynchronize(); // ? Is it not reqd wo streams?
 
 		while(!*isAllNodesTraversed) {
 
 			*isAllNodesTraversed = true;
 			BreadthFirstSearch<<<blocks, threads>>>(
-				V, d_meta, d_data, d_delta, d_sigma,
+				V, d_meta, d_data, d_sigma,
 				d_d, level, isAllNodesTraversed
 			);
 
@@ -204,36 +214,37 @@ double* ComputeBetweennessCentrality(graph& g, vector<int>& sources) { // TODO: 
 		}
 
 		(*level)--;
-		cout << "Sigma values for src " << i << " ";
-		PrintArray<<<1,1>>>(d_sigma, V);
-		hipDeviceSynchronize();
 
-		while(*level > 1) {
+		InitializeArray<double><<<blocks, threads>>>(d_delta, 0.0, V);
+
+		while(*level > 0) {
 			
-			// cout << "Inside ReversePass loop. level : " << *level << NL;
 			ReversePass<<<blocks, threads>>>(
 				V, d_meta, d_data, d_delta, d_sigma,
 				d_d, level, isAllNodesTraversed, d_bc
 			); 
 			
-			hipDeviceSynchronize(); //? Problem with grid.sync()
+			//? Problem with grid.sync()
 
-			AddElements<<<blocks, threads>>>(
-				V, d_bc, d_delta
+			AddElements<<<blocks, threads>>>( 
+				V, d_bc, d_delta, d_d, level, s
 			);
 
+			hipDeviceSynchronize(); //? Problem with grid.sync()
+			
 			(*level)--;
 		}
 		hipDeviceSynchronize();
 	}
 
-	// TODO: TIMING ENDS
+	hipEventRecord(stop,0);
+	hipEventSynchronize(stop);
+	hipEventElapsedTime(&milliseconds, start, stop);
+	printf("GPU Time: %.6f ms\n", milliseconds);
 
 	hipMemcpy(h_bc, d_bc, sizeof(double) * V, hipMemcpyDeviceToHost);
 
 	//! TODO: Free up memory
-
-	return h_bc;
 }
 
 int main(int argc, char* argv[]) {
@@ -256,7 +267,8 @@ int main(int argc, char* argv[]) {
 
 	srcfile.close();
 
-	double* bc = ComputeBetweennessCentrality(G, src);
+	double *bc = (double*) malloc(sizeof(double) * G.num_nodes());
+	ComputeBetweennessCentrality(G, src, bc);
 
 	// Process bc data
 
