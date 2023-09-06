3cfda29348f1c73654400164a7c9e1d761381758
Barenya255 <bknandy98@gmail.com>
2023-09-06 23:44:17 +0530

    added push and relabel methods

--- max-flow/serial_correct/max_flow.hpp
+++ max-flow/serial_correct/max_flow.hpp
@@ -13,6 +13,7 @@ struct edge_elements {
   // struct to store entities relevant to an edge for the max flow problem.
   int flow ; // current flow value through the edge.
   int capacity ; // capacity of the edge.
+  int residual ; // To represent the residual graph.
   vertex_properties *source; // vertex type struct containing the source vertex of the edge.
   vertex_properties *destination ; // vertex type struct containing the destination vertex of the edge.
 
@@ -45,6 +46,7 @@ struct vertex_properties {
 
 bool operator< (const vertex_properties &a, const vertex_properties &b) {
 
+  // overload comparator so that vertex_properties may be used in the map.
   return a.vertex_id < b.vertex_id ;
 }
 
@@ -64,6 +66,9 @@ private:
   // source and sink to be defined by user. 
   vertex_properties source ;
   vertex_properties sink ;
+
+  // The adjacency list of the graph.
+  // TO:DO - Change to CSR/ unordered_map post discussion.
   map <vertex_properties, vector<edge_elements> > adj;
 
 public:
@@ -83,60 +88,71 @@ public:
   }
 
 
-  void set_up_excess () {
-
-
-    // check whether source and sink are set or not.
-    assert (source.vertex_id != -1) ;
-    assert (sink.vertex_id != -1) ;
-    
-
-    // set excess of source to be the sum of all capacities going out of the source.. 
-    source.excess = 0;
-  }
-
   void initialize () {
     /*
      * initialize the adjacency list of the array and create the max flow.*/
     
+
+    // retreive edge list from the parsed graph. It is actually sent back as an adjacency list for some reason.
     map <int, vector<edge>> adj_list = getEdges();   
 
     // TO:DO change find a better way to represent the adjacency list. 
     for (auto edge:adj_list) {
     
+      // for every vertex in adjacency list (confusion caused because of naming conventions prior).
       edge_elements edge_elements_new;
   
+
+      // Make the vertex properties struct for current vertex. Initialize the the members of the struct.
       vertex_properties this_vertex;
       this_vertex.vertex_id = edge.first;
       this_vertex.active = true;
-      this_vertex.excess=0;
+      this_vertex.excess = 0;
 
+      // Create empty vector just in case this is not supported.
       if (adj.find (this_vertex) == adj.end ()) {
         adj[this_vertex] = {};
       }
 
-     for (auto edge_props:edge.second) {
+      // Fill up the adjacency list (vector) for the current vertex.
+      for (auto edge_props:edge.second) {
 
 
+        // create a destination vertex, initialize all its members.
         vertex_properties destination ;
         destination.excess = 0 ;
         destination.active = true ;
         destination.vertex_id = edge_props.destination ;
 
+
+        // initialize the edge struct and all its members.
         edge_elements_new.flow = 0 ;
         edge_elements_new.capacity = edge_props.weight ;
         edge_elements_new.destination = &destination ;
 
+        // Add the edge to the adjacency matrix.
         adj[this_vertex].push_back (edge_elements_new) ;
          
       }
     }
     // End To:DO
 
+    // initialize the heights and the excesses.
     set_up_heights () ;
     set_up_excess () ;
   }
 
+  void set_up_excess () {
+
+    
+    // set up the excesses. initialize source to be sum of all capacities outgoing and all others to 0.
+    for (auto v_edge:adj[source]) {
+      vertex_properties v= *v_edge.destination ;
+      source.excess += v_edge.capacity ;
+    }
+
+    // all else is already at 0 ;
+  }
   void set_up_heights () {
 
     /* 
@@ -146,8 +162,8 @@ public:
     q.push (source) ;
     vector<vertex_properties> visited (num_nodes ()) ;
 
-
-
+    
+    // Run the BFS for loop.
     while (!q.empty ()) {
       
       vertex_properties u = q.front () ;
@@ -159,7 +175,7 @@ public:
         // Therefore, must retreive the destination vertex from it.
         vertex_properties v = *(v_edge.destination) ;
 
-        
+       // To Do : Fix the [] operator overlaod. For some reason, this is not working 
         if (!visited[v]) { 
 
 
@@ -169,6 +185,64 @@ public:
         } 
       }
     }
+  }
+
+  
+
+  void relabel (vertex_properties relabel_this_vertex) {
+    
+    // relabel the passed vertex.. Change it's height to the lowest height among it's adjacent vertics increased by 1
+    //
+    int minim = 1000000000 ;
+    for (auto edge : adj[relabel_this_vertex]) {
+
+      minim = min (minim, edge.destination->height+1) ;
+    }
+    relabel_this_vertex.height = minim ;
+  }
+
+
+
+  void push (vertex_properties active_vertex) {
+
+
+    // check whether the current edge is still a valid edge to push heights to.
+    if (active_vertex.height != active_vertex.current_edge->destination->height+1) {
+
+      // if not reset the current edge for the current vertex.
+      reset_current_edge (active_vertex) ;
+    }
+    // push flow from a particular vertex to all subsequent vertices .. 
+    int curr_flow = min (active_vertex.excess, active_vertex.current_edge->flow) ;
+
+    // update all relevant values
+    active_vertex.current_edge->residual += curr_flow ;
+    active_vertex.current_edge->flow -= curr_flow ;      
+    active_vertex.excess -= curr_flow ;
+
+    // TO:DO 
+    // When to Deactivate the Vertex ?? 
+  }
+
+
+
+  void reset_current_edge (vertex_properties active_vertex) {
+
+
+    // search for edges leading to vertices with height exactly lesser by 1.
+    for (auto v_edge:adj[active_vertex]) {
+
+      // check whether v_edge satisfies the property.
+      if (v_edge.destination->height == active_vertex.height+1) {
+
+        // update the active vertex's edge.
+        active_vertex.current_edge = &v_edge ;
+        return ;
+      }
+    }
+
+    // if control reaches here, relabelling is needed .
+    relabel(active_vertex) ;
   }
 };
 
