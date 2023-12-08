5a1f9e7ff8d38e5415ea007de541f49ef4ea45e3
Barenya255 <109261257+Barenya255@users.noreply.github.com>
2023-12-08 23:37:10 +0530

    Create main.cpp
    added main.cpp to the website directly. There are merge conflicts

---
+++ max-flow/scratch/main.cpp
@@ -0,0 +1,294 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector> 
+#include <queue>
+#include <chrono>
+#include <climits>
+#include <cassert>
+#include <tuple>
+#include <ctime> 
+#include <iomanip>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<vi> vvi ;
+typedef pair<ll,ll> ii ;
+typedef vector<ii> vii ;
+typedef vector<vvi> vvii ;
+typedef queue<ll> q ;
+
+#define FORWARD 0 
+#define BACKWARD 1 
+
+
+const int no_debug = 1 ;
+const int log_emergency=1 ;
+const int evaluate_mode = 0 ;
+vi heights ;
+vi excess ;
+vi current_arc ;
+vvi graph ;
+vvii residual_graph ;
+vvi edges ;
+q active ;
+int vx, e, src, snk ;
+int counter_to_global_relabel=0 ;
+
+void fileIO(){
+    freopen("input.txt", "r", stdin);
+}
+
+void log_graph (vvii &a) {
+    if (evaluate_mode) return ;
+    if (no_debug) return ;
+    for (int i=0; i<a.size (); i++) {
+        for (auto &it1:a[i]) {
+            cerr <<i << " : " << it1[0] << " " << it1[1] << " " << it1[2] << " " << it1[3] << endl;
+        }
+    }
+}
+
+void log_arr (vi &a) {
+    if (evaluate_mode) return ;
+    if (no_debug) return ;
+    for (auto &it:a) {
+        cerr << it << " " ;
+    }
+    cerr << endl ;
+}
+
+void log_message (const string &msg) {
+    if (evaluate_mode) return ;
+     if (no_debug) return ;
+    cerr << msg << endl ;
+}
+
+void log_message_push (const string &msg) {
+    //  if (no_debug) return ;
+    if (evaluate_mode) return ;
+    cerr << msg << endl ;
+}
+
+void log_sos (const string &msg) {
+    if (evaluate_mode) return ;
+	if (!log_emergency) return ;
+	cout << msg << endl ;
+}
+
+void input (FILE* graph_file) {
+
+    string s ;
+    vx = 0, e = 0 ;
+    int u, v, w ;
+    while (fscanf (graph_file, "%d %d %d", &u, &v, &w) == 3) {
+
+        edges.push_back ({u,v,w}) ;
+        vx = max (vx, v) ;
+        vx = max (vx, u) ;
+        e++ ;
+    }
+    vx++ ;
+
+    log_message ("input successful") ;
+	log_message (" number of vertices : " + to_string (vx)) ;
+    
+    residual_graph.resize (vx) ;
+	log_sos ("raw input OK") ;
+
+    for (auto &edge:edges) {
+        residual_graph[edge[0]].push_back ({edge[1], edge[2], (ll)residual_graph[edge[1]].size (),FORWARD}) ;
+        residual_graph[edge[1]].push_back ({edge[0], 0, (ll)residual_graph[edge[0]].size ()-1ll, BACKWARD}) ;
+    }
+    log_sos ("graph build successful") ;
+}
+
+void bfs_label () {
+
+	log_message ("starting bfs : " + to_string (heights.size ())) ;
+    queue<pair<int,int> > q ;
+    q.push ({0,snk}) ;
+    vector<int> visited (vx, 0) ;
+    int max_level = 0 ;
+    while (!q.empty()) {
+        int level, u ;
+        tie (level, u) = q.front () ;
+        max_level = max (max_level, level) ;
+        q.pop () ;
+		if (visited[u]) continue ;
+		log_message ("heighst.size () = " + to_string (heights.size ())) ;
+        log_message ("searching through " + to_string (u)) ;
+        log_message ("assigning: " + to_string(u) + " " + to_string(heights[u])) ;
+        visited[u]=1 ;
+        heights[u]=level ;
+
+        for (auto &x:residual_graph[u]) {
+            int v = x[0] ;
+            int w = x[1] ;
+            int offset = x[2] ;
+			int capacity = residual_graph[v][offset][1] ;
+
+            // cannot find appropriate representation for flow and reverse flow.
+            // Made graph according to residual capacity.
+
+            log_message ("for " + to_string(u) + "->" + to_string(v) + ", flow = " + to_string (capacity - w)) ;
+            log_message ("for " + to_string(v) + "->" + to_string(u) + ", flow = " + to_string (capacity - w)) ;
+
+
+            if (residual_graph[v][offset].back () == BACKWARD && !visited[v] && w>0) {
+                q.push ({level+1, v}) ;
+            }
+        }
+    }
+    heights[src] = max_level+1 ;
+    log_message ("bfs labeled heights : " ) ;
+    log_arr (heights) ;
+	// log_sos ("did bfs again") ;
+}
+
+void max_flow_init ()  {
+
+    heights.assign (vx,0) ;
+    excess.assign (vx, 0) ;
+    current_arc.assign (vx, 0) ;
+}
+
+void set_source_sink (const int &source, const int &sink) {
+
+    src = source ;
+    snk = sink ;
+
+    log_graph (residual_graph) ;
+
+    heights[src] = vx ;
+    for (int v_idx = 0 ; v_idx < residual_graph[src].size (); v_idx++ ){
+        int v = residual_graph[src][v_idx][0] ;
+        residual_graph[v][residual_graph[src][v_idx][2]][1] += residual_graph[src][v_idx][1] ;
+        excess[v] += residual_graph[src][v_idx][1] ;
+        residual_graph[src][v_idx][1] = 0 ;
+        if (excess[v] > 0 && v!= src && v!=snk) {
+            active.push (v) ;
+        }
+    }
+    excess[src] = 0 ;
+}
+
+bool relabel (int u) {
+
+    log_message_push ("relabeling " + to_string (u) + " at height " + to_string (heights[u])) ;
+    log_message ("=======================================") ;
+    log_arr (heights) ;
+    log_arr (excess) ;
+    log_graph (residual_graph) ;
+    log_message ("=======================================") ;
+
+    assert (excess[u]>0 ) ;
+    ll minim = INT_MAX ;
+    for (int v=0; v<residual_graph[u].size (); v++) {
+        if (residual_graph[u][v][1]>0 ) {
+            assert (heights[u] <= heights[residual_graph[u][v][0]]) ;
+            minim = min (minim, heights[residual_graph[u][v][0]]) ;
+        }
+    }
+    heights[u] = minim + 1 ;
+    log_message_push ("new height : " + to_string (heights[u])) ;
+    return true ;
+}
+
+
+bool push (const int &u, const int &v) {
+
+    assert (excess[u] > 0 && (heights[u] >= heights[v] + 1) ); 
+    ll delta = min (excess[u], residual_graph[u][current_arc[u]][1]) ;
+    log_message_push ("pushing flow from " + to_string(u) + " to " + to_string (v) + " of val " + to_string (delta)) ;
+    log_message_push ("heights : " + to_string (heights[u]) + " and " + to_string(heights[v])) ;
+    residual_graph[u][current_arc[u]][1]-=delta ;
+    log_graph (residual_graph) ;
+    residual_graph[v][residual_graph[u][current_arc[u]][2]][1]+=delta ;
+    excess[v]+=delta ;
+    excess[u]-=delta ;
+    if (excess[v]>0 && (v != src) && (v != snk) && excess[v] == delta) {
+        log_message(  "pushing " + to_string(v) + " into active "  );
+        active.push(v) ;
+    }
+    return true ;
+}
+
+void discharge (const int &u) {
+
+    while (excess[u]>0) {
+        
+ 
+        log_sos (to_string (active.size ()) + " " + to_string(active.front ()) + " " + to_string (excess[active.front ()])) ;
+        
+		if (counter_to_global_relabel==15) {
+			counter_to_global_relabel=0 ;
+            // log_sos (to_string (active.size ()) + " " + to_string(active.front ()) + " " + to_string (excess[active.front ()])) ;
+			bfs_label () ;
+		}
+
+        if (current_arc[u] < residual_graph[u].size ()) {
+            int v = residual_graph[u][current_arc[u]][0];
+            if (residual_graph[u][current_arc[u]][1] > 0 && heights[u] >= heights[v] + 1)
+                push(u, v);
+            else 
+                current_arc[u]++;
+        } else {
+            relabel(u);
+            current_arc[u] = 0;
+        }
+		counter_to_global_relabel++ ;
+    }
+}
+
+void max_flow () {
+
+    while (!active.empty () ) {
+
+        int u = active.front () ;
+        active.pop () ;
+        if (u!=src && u!=snk) 
+        discharge (u) ;
+        log_arr (excess) ;
+        log_arr (heights) ;
+    }
+}
+
+int main (int argc, char** argv) {
+    //fileIO();
+	char* file_path = argv[1] ;
+	FILE* graph_file = fopen (file_path, "r") ;
+	log_sos (file_path) ;
+	if (graph_file == NULL) {
+		log_sos ("FILE NOT FOUND") ;
+		return 1 ;
+	}
+    auto start_tick = std::chrono::steady_clock::now () ;
+	log_sos ("starting the code") ;
+    input (graph_file) ;
+    auto input_done_tick = std::chrono::steady_clock::now () ;
+    auto duration = std::chrono::duration_cast <std::chrono::microseconds> (input_done_tick-start_tick) ;
+    log_sos ("Input time : " +  to_string(duration.count ()) + " us") ;
+	log_sos ("input OK") ;
+    max_flow_init () ;
+	log_sos ("init OK") ;
+    int source = 0 ;
+    int sink = 1 ;
+    set_source_sink (source, sink) ;
+	log_sos ("source and sink OK ") ;
+    bfs_label () ;
+    auto bfs_tick = std::chrono::steady_clock::now () ;
+    duration = std::chrono::duration_cast <std::chrono::microseconds> (bfs_tick-start_tick) ;
+    log_sos ("bfs time : " +  to_string(duration.count ()) + " us") ;
+	log_sos ("bfs labelling OK " ) ; 
+    max_flow () ;
+	log_sos ("max flow successful ") ; 
+    auto end_tick = std::chrono::steady_clock::now () ;
+    duration = std::chrono::duration_cast <std::chrono::microseconds> (end_tick-start_tick) ;
+    log_sos ("total time : " + to_string (duration.count()) + " us") ;
+    log_arr (excess) ;
+    cout << excess[sink] << endl ;
+  return 0;
+}
