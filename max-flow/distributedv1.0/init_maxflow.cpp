44429d66b325e121076456f479e6cf25fbd6ec0d
Barenya255 <109261257+Barenya255@users.noreply.github.com>
2023-12-12 14:26:40 +0530

    init_maxflow with working inputting

--- max-flow/distributedv1.0/init_maxflow.cpp
+++ max-flow/distributedv1.0/init_maxflow.cpp
@@ -7,33 +7,40 @@
 
 
 #include "partitioned_file_reader.hpp"
-#include "macro_defs.hpp"
 #include <mpi.h>
 #include <assert.h>
-#include "max_flow_csr.hpp"
-#include "pushers.hpp"
-#include "relabelers.hpp" 
+#include "Distributed_Network.hpp"
 
 int source_process, sink_process ;
 bool ACTIVE_VERTEX_EXISTS_GLOBAL ;
 
 int main (int argc, char** argv) {
 
-  int status = MPI_Init (&argc, &argv) ;
 
-  assert (status == MPI_SUCCESS) ;
+    log_message ("program started") ;
+    int status = MPI_Init (&argc, &argv) ;
+    assert (status == MPI_SUCCESS) ;
 
-  int source, sink, source_process, sink_process ;
+    int source, sink, source_process, sink_process ;
 
-  printf ("Enter source and sink nodes \n") ;
-  scanf ("%d %d ", &source, &sink) ;
+    source = 0, sink = 1 ;
 
- // Network_flow curr_network = read_current_file (MPI_COMM_WORLD, source, sink, ACTIVE_VERTEX_EXISTS_GLOBAL) ;
+    int p_no, size ;
+    MPI_Comm_size (MPI_COMM_WORLD, &size) ;
+    MPI_Comm_rank (MPI_COMM_WORLD, &p_no) ;
 
-//  tie (source_process, sink_process) = find_source_sink (curr_network) ;
+    log_message ("Process id = " + to_string (p_no) + "\n world size = " + to_string (size))  ;
 
-  //while (ACTIVE_VERTEX_EXISTS_GLOBAL) {
+    char* local_graph_path = construct_file_name (p_no) ;
     
-  //}
-  MPI_Finalize () ;
+    printf ("name = %s\n", local_graph_path) ;
+    Distributed_Network max_flow_network (local_graph_path, source, sink, p_no, size) ;
+    free (local_graph_path) ;
+    local_graph_path = NULL ;
+    while (1) {
+        max_flow_network.discharge_active_vertices () ;
+        max_flow_network.sync_active_vertices () ;
+    }
+    MPI_Finalize () ;
+    return 0 ;
 }
