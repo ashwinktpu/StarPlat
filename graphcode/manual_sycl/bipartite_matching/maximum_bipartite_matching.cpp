07662e20507020c9098e9a86af3995685bd02bbb
Ayush Kumar Mall <ayushmall06@gmail.com>
2024-01-24 14:40:27 +0530

    Add Utility Functions

--- graphcode/manual_sycl/bipartite_matching/maximum_bipartite_matching.cpp
+++ graphcode/manual_sycl/bipartite_matching/maximum_bipartite_matching.cpp
@@ -8,7 +8,71 @@ using namespace sycl;
 const int NUM_THREADS = 1048576;
 const int STRIDE = NUM_THREADS;
 
+//-------------------------- Utility Functions -------------------------//
 
+void memoryCopy(queue &Q, bool *to, bool *from, int memSize) {
+    Q.submit([&](handler &h) {
+        h.memcpy(to, from, memSize*sizeof(bool));
+    }).wait();
+}
+
+void memoryCopy(queue &Q, int *to, int *from, int memSize) {
+    Q.submit([&](handler &h) {
+        h.memcpy(to, from, memSize*sizeof(int));
+    }).wait();
+}
+
+void init(queue &Q, bool *arr, bool val, int arrLen) {
+    if(arrLen == 1) {
+        Q.submit([&](handler &h) {
+            h.single_task([=]() {
+                arr[0] = val;
+            });
+        }).wait();
+    } else {
+        Q.submit([&](handler &h) {
+            h.parallel_for(NUM_THREADS, [=](id<1> i) {
+                for(int v = i; v < arrLen; v += STRIDE) {
+                    arr[v] = val;
+                }
+            });
+        }).wait();
+    }
+}
+
+void init(queue &Q, int *arr, int val, int arrLen) {
+    if(arrLen == 1) {
+        Q.submit([&](handler &h) {
+            h.single_task([=]() {
+                arr[0] = val;
+            });
+        }).wait();
+    } else {
+        Q.submit([&](handler &h) {
+            h.parallel_for(NUM_THREADS, [=](id<1> i) {
+                for(int v = i; v < arrLen; v += STRIDE) {
+                    arr[v] = val;
+                }
+            });
+        }).wait();
+    }
+}
+
+int* hostMemAllocateInteger(int len) {
+    return (int *)malloc((len)*sizeof(int));
+}
+
+bool* hostMemAllocateBool(int len) {
+    return (bool *)malloc((len)*sizeof(bool));
+}
+
+int* deviceMemAllocateInteger(queue &Q, int len) {
+    return malloc_device<int>(len, Q);
+}
+
+bool* deviceMemAllocateBool(queue &Q, int len) {
+    return malloc_device<bool>(len, Q);
+}
 
 
 int main(int argc, char** argv) {
