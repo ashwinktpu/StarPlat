3db4c65440b7c1bfcadfae53492f27e8adc99cdc
Ayush Kumar Mall <ayushmall06@gmail.com>
2024-01-24 14:37:34 +0530

    Add Input Handling Code

---
+++ graphcode/manual_sycl/bipartite_matching/maximum_bipartite_matching.cpp
@@ -0,0 +1,22 @@
+#include<bit/stdc++.h>
+#include<CL/sycl.hpp>
+#include "graph.hpp"
+using namespace sycl;
+
+int main(int argc, char** argv) {
+    char* inp = argv[1];
+    bool isWeighted = atoi(argv[2]) ? true : false;
+    printf("Taking input from: %s\n", inp); fflush(stdout);
+
+    graph g(inp);
+    g.parseGraph(isWeighted);
+
+    int nc = 0;
+    printf("If left partition size is not given it takes n/2 by default\n"); fflush(stdout);
+    if(argc == 4) nc = atoi(argv[3]);
+    else nc = g.num_nodes()/2;
+    printf("Left partition size: %d\n", nc); fflush(stdout);
+
+    APFB(g, nc);
+    return 0;
+}
